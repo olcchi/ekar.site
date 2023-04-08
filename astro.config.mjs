26a9cacea86021ccf6f6c5b509459a40934916e8
EKar <Earthb0y@outlook.com>
2023-04-08 23:11:14 +0800

    feat: add headless cms : storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import unocss from 'unocss/astro'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    unocss(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'us', // optional,  or 'eu' (default)
+      },
+    }),
+  ],
+})
