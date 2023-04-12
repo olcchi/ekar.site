448eaf26efa28a2340165a45a67560e9d26f8bf4
EKar <Earthb0y@outlook.com>
2023-04-12 14:44:36 +0800

    chore: Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import unocss from 'unocss/astro'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     unocss(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-        blogPost: 'storyblok/BlogPost',
-        blogPostList: 'storyblok/BlogPostList',
-        page: 'storyblok/Page',
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: 'us', // optional,  or 'eu' (default)
-      },
-    }),
   ],
 })
