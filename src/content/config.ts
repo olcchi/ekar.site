1fe95e5480108a4277beeddf7022b3d27a29051c
EKar <Earthb0y@outlook.com>
2023-04-13 19:35:17 +0800

    fix: CollectionSchema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection } from 'astro:content'
 import { postsSchema } from './postsSchema'
-const postCollection2022 = defineCollection(postsSchema)
-const postCollection2023 = defineCollection(postsSchema)
+const postCollection2022 = defineCollection({ schema: postsSchema })
+const postCollection2023 = defineCollection({ schema: postsSchema })
 
 export const collections = {
   2022: postCollection2022,
