011c86e5f20803b2800e73fe901ccce27fc60e8a
EKar <Earthb0y@outlook.com>
2023-04-13 19:14:45 +0800

    chore: PresetWebFonts& PresetTransformerDirectives

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,6 +6,7 @@ import {
   presetUno,
   presetWebFonts,
 } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   presets: [
@@ -13,20 +14,27 @@ export default defineConfig({
     presetUno(),
     presetAttributify(),
     presetWebFonts(
-      // {
-      //   provider: 'bunny',
-      //   fonts: {
-      //     serif: 'Alice',
-      //   },
-      // },
+      {
+        provider: 'fontshare',
+        fonts: {
+          serif: 'Telma',
+        },
+      },
     ),
     presetTypography(),
   ],
   shortcuts: [
     {
-      defaultDark: 'dark:bg-dark-800 dark:text-white transition-all ease-in-out duration-200',
+      defaultDark: 'dark:bg-dark-800 dark:text-gray-2 transition-colors ease-in-out duration-200',
       defaultLight: 'bg-white text-black transition-all ease-in-out duration-200',
       defaultAnimation: 'transition-all duration-200 ease-in-out',
+      defaultBorder: 'dark:border-dark-2 border-gray-2',
+      defaultLayer: 'w-5/6 lg:w-1/2 xl:w-1/3',
     },
+  ],
+  transformers: [
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    transformerDirectives(),
   ],
 })
