304988cf8fd3e48d4a3345c84379fbe92628d83b
EKar <Earthb0y@outlook.com>
2023-04-13 19:15:00 +0800

    chore: Remove React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
 import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
     unocss(),
   ],
 })
