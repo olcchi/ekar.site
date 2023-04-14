223523c6a444650ad5f0fe0f21f6f7089c1fed68
EKar <Earthb0y@outlook.com>
2023-04-14 15:16:33 +0800

    fix: Animation logic

--- src/utils/useAnimation.ts
+++ src/utils/useAnimation.ts
@@ -1,25 +1,24 @@
 import { gsap } from 'gsap'
-export const prerender = true
 export const useAnimation = (query: 'posts' | 'postsList' | 'index'): void => {
   switch (query) {
     case 'posts':
       return (() => {
         const tl = gsap.timeline({ defaults: { duration: 0.5 } })
-        tl.from('#backToWrapper', { translateY: 12, opacity: 0, delay: 0.2 })
-          .from('#postsWrapper', { translateY: 12, opacity: 0 })
+        tl.to('#backToWrapper', { y: 0, opacity: 1, delay: 0.2 })
+          .to('#postsWrapper', { y: 0, opacity: 1 })
       })()
     case 'postsList':
       return (() => {
-        const tl = gsap.timeline({ defaults: { duration: 0.5, opacity: 0 } })
-        tl.from('#backToWrapper', { translateY: 12, delay: 0.2 })
-          .from('#postsListWrapper', { translateY: 12 })
+        const tl = gsap.timeline({ defaults: { duration: 0.5 } })
+        tl.to('#backToWrapper', { y: 0, opacity: 1, delay: 0.2 })
+          .to('#postsListWrapper', { y: 0, opacity: 1 })
       })()
     case 'index':
       return (() => {
         const tl = gsap.timeline({ defaults: { duration: 0.5, ease: 'power2' } })
-        tl.from('#aboutMe', { opacity: 0, rotationX: 90, delay: 0.2 })
-          .from('#Project', { opacity: 0, rotationX: 90 })
-          .from('#Now', { opacity: 0, rotationX: 90 })
+        tl.to('#aboutMe', { opacity: 1, y: 0, delay: 0.2 })
+          .to('#Project', { opacity: 1, y: 0 })
+          .to('#Now', { opacity: 1, y: 0 })
       })()
     default:
       throw new Error(`Invalid query Value ${query} `)
