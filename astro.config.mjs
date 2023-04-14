2e84b608804f42005fb34450b510ce97c80dcd4a
EKar <Earthb0y@outlook.com>
2023-04-14 10:13:53 +0800

    chore: Server Side Rendering @ Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
 
+import netlify from '@astrojs/netlify/functions'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss(),
-  ],
+  integrations: [unocss()],
+  server: {
+    port: 8000,
+    host: true,
+  },
+  output: 'server',
+  adapter: netlify(),
 })
