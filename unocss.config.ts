560376dd631be340d399247d66015256ab991c4d
EKar <Earthb0y@outlook.com>
2023-04-17 18:56:20 +0800

    feat(ui): Keyframe Animation

--- unocss.config.ts
+++ unocss.config.ts
@@ -26,9 +26,9 @@ export default defineConfig({
   shortcuts: [
     {
       defaultDark: 'dark:bg-dark-800 dark:text-gray-2 transition-colors ease-in-out duration-200',
-      defaultLight: 'bg-white text-black transition-all ease-in-out duration-200',
+      defaultLight: 'bg-white text-black transition-colors ease-in-out duration-200',
       defaultAnimation: 'transition-all duration-200 ease-in-out',
-      defaultBorder: 'dark:border-dark-2 border-gray-2',
+      defaultBorder: 'dark:border-dark-2 border-gray-2 ',
       defaultLayer: 'w-5/6 lg:w-1/2 xl:w-1/3',
     },
   ],
@@ -37,4 +37,18 @@ export default defineConfig({
     // @ts-expect-error
     transformerDirectives(),
   ],
+  theme: {
+    animation: {
+      keyframes: {
+        slideInUp: '{ from {transform: translateY(10px);opacity:0;} to {transform: translateY(0px);opacity:100;}}',
+      },
+      durations: {
+        slideInUp: '0.8s',
+      },
+      timingFns: {
+        slideInUp: 'ease',
+      },
+    },
+  },
+
 })
