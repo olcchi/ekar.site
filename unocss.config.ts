3020565b58125b78b236966535a762c164bc63c6
EKar <Earthb0y@outlook.com>
2023-04-20 19:05:32 +0800

    fix: Default Layer Width

--- unocss.config.ts
+++ unocss.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       defaultLight: 'bg-white text-black transition-colors ease-in-out duration-200',
       defaultAnimation: 'transition-all duration-200 ease-in-out',
       defaultBorder: 'dark:border-dark-2 border-gray-2 ',
-      defaultLayer: 'w-5/6 lg:w-1/2 xl:w-1/3',
+      defaultLayer: 'w-5/6 lg:w-1/2 xl:w-2/5',
     },
   ],
   transformers: [
