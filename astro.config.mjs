f45b235ad6824eb1e4767a13e2d266b7a615cbe6
EKar <Earthb0y@outlook.com>
2023-04-20 19:03:54 +0800

    feat: site url & Remark Plugin Reading Time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
+import { remarkReadingTime } from './remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ekar.site',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   integrations: [unocss()],
   server: {
     port: 8000,
