115977594a6f233ac47600ec94001ccfedd7406c
EKar <Earthb0y@outlook.com>
2023-04-20 19:10:01 +0800

    feat: Content Schema : Collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,22 @@
+// https://docs.astro.build/zh-cn/guides/content-collections/
 import { defineCollection, z } from 'astro:content'
+
 const postCollection2022 = defineCollection({
   schema: z.object({
     title: z.string(),
     pubDate: z.string(),
+    collection: z.number(),
     description: z.string().optional(),
+    readingTime: z.number().optional(),
   }),
 })
 const postCollection2023 = defineCollection({
   schema: z.object({
     title: z.string(),
     pubDate: z.string(),
+    collection: z.number(),
     description: z.string().optional(),
+    readingTime: z.number().optional(),
   }),
 })
 
