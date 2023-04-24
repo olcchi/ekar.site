9837a4efe4b0c40a4b79188233fda4176a4797d6
EKar <Earthb0y@outlook.com>
2023-04-24 22:38:36 +0800

    chore: shortcuts

--- unocss.config.ts
+++ unocss.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       defaultLight: 'bg-white text-black ',
       defaultBorder: 'dark:border-dark-2 border-gray-2 ',
       defaultLayer: 'w-5/6 lg:w-1/2 xl:w-2/5',
-      defaultLink: ' decoration-none border-b border-gray-3 dark:border-dark-1 hover:border-gray-8 dark:hover:border-gray-3',
+      defaultLink: ' border-b border-gray-3 dark:border-dark-1 hover:border-gray-8 dark:hover:border-gray-3',
       MdLink: 'decoration-none border-b hover:border-gray-3 dark:hover:border-dark-1 border-gray-8 dark:border-gray-3',
     },
   ],
