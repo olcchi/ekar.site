f09812ed5b50b965581c78cad02e0ed7ece08a39
EKar <Earthb0y@outlook.com>
2023-05-07 10:20:23 +0800

    refactor: Shortcuts rename

--- unocss.config.ts
+++ unocss.config.ts
@@ -27,12 +27,10 @@ export default defineConfig({
   ],
   shortcuts: [
     {
-      defaultDark: 'dark:bg-black dark:text-gray-2',
-      defaultLight: 'bg-white text-black ',
-      defaultBorder: 'dark:border-dark-2 border-gray-2 ',
-      defaultLayer: 'w-5/6 lg:w-1/2 xl:w-2/5',
-      defaultLink: 'block decoration-none border-b border-gray-3 dark:border-dark-1 hover:border-gray-8 dark:hover:border-gray-3',
-      MdLink: 'decoration-none border-b hover:border-gray-3 dark:hover:border-dark-1 border-gray-8 dark:border-gray-3',
+      themeBase: 'bg-white text-black dark:bg-black dark:text-gray-2 ',
+      borderBase: 'dark:border-dark-2 border-gray-2 ',
+      linkBase: 'block decoration-none border-b border-gray-3 dark:border-dark-1 hover:border-gray-8 dark:hover:border-gray-3',
+      linkMd: 'decoration-none border-b hover:border-gray-3 dark:hover:border-dark-1 border-gray-8 dark:border-gray-3',
     },
   ],
   transformers: [
@@ -41,13 +39,13 @@ export default defineConfig({
   theme: {
     animation: {
       keyframes: {
-        slideInUp: '{ from {transform: translateY(10px);opacity:0;} to {transform: translateY(0px);opacity:100;}}',
+        slideUp: '{ from {transform: translateY(10px);opacity:0;} to {transform: translateY(0px);opacity:100;}}',
       },
       durations: {
-        slideInUp: '0.8s',
+        slideUp: '0.8s',
       },
       timingFns: {
-        slideInUp: 'ease',
+        slideUp: 'ease',
       },
     },
   },
