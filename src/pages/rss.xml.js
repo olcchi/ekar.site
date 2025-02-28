f0a5023e25f3ca0df6bba7af05f9c04d3daa7b59
ekar <earthb0y@outlook.com>
2025-02-28 23:34:05 +0800

    chore : rss configuration

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
-export async function get(context) {
+export async function GET(context) {
   const collection2022 = await getCollection('2022')
   const collection2023 = await getCollection('2023')
   const posts = [...collection2022, ...collection2023]
